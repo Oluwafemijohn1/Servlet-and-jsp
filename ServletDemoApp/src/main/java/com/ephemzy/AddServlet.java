9f40c1b94982bc5aa9e2952a77a0c45ab77ca8c9
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-03-26 06:10:23 +0100

    built servlet

---
+++ ServletDemoApp/src/main/java/com/ephemzy/AddServlet.java
@@ -0,0 +1,24 @@
+package com.ephemzy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num1"));
+		int k = i + j;
+		
+		PrintWriter out = resp.getWriter();
+		out.println("Result" + k);
+	}
+	
+
+}
