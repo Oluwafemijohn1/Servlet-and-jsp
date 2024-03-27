a69a10f1c47893ae4198aa78c0061b57d44cfbc7
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-03-27 16:45:16 +0100

    Learnt how to use servlet annotation

--- ServletDemoApp/src/main/java/com/ephemzy/AddServlet.java
+++ ServletDemoApp/src/main/java/com/ephemzy/AddServlet.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
+@WebServlet("/add")
 public class AddServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
